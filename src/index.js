541859fad4c318c17bfc9bccf65077323bebc818
Krushna <krushnamk1009@gmail.com>
2022-05-19 12:27:30 +0530

    succesfully commited

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app";
+
+// const Heading = () => {
+//   return <h1>My Favourite Foods</h1>;
+// };  t
+
+ReactDOM.render(<App />, document.getElementById("root"));
